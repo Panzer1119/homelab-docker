ecabc8dbe71b5d324e3b99f5646183fac71bf9dd
Paul Hagedorn <panzer1119@gmail.com>
2024-07-25 08:08:53 +0200

    feat: improve process_docker_cifs_volumes.sh

--- scripts/process_docker_cifs_volumes.sh
+++ scripts/process_docker_cifs_volumes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+CREATE_CIFS_VOLUME_SCRIPT_FILENAME="create_docker_cifs_volume.sh"
+
+CREATE_CIFS_VOLUME_SCRIPT_FILE="${PWD}/${CREATE_CIFS_VOLUME_SCRIPT_FILENAME}"
+
 # Function to display usage
 usage() {
   echo "Usage: ${0} -d <directory> -a <address> -u <username> -p <password> [-q]"
@@ -8,6 +12,7 @@ usage() {
   echo "  -u <username>: Username for authentication"
   echo "  -p <password>: Password for authentication"
   echo "  -q: Optional. Quiet mode"
+  echo "  -n: Optional. Dry run"
   exit 1
 }
 
@@ -18,7 +23,7 @@ if ! command -v yq &> /dev/null; then
 fi
 
 # Parse command line arguments
-while getopts "d:a:u:p:" opt; do
+while getopts "d:a:u:p:qn" opt; do
   case ${opt} in
     d )
       directory=$OPTARG
@@ -31,6 +36,12 @@ while getopts "d:a:u:p:" opt; do
       ;;
     p )
       password=$OPTARG
+      ;;
+    q )
+      QUIET=1
+      ;;
+    n )
+      DRY_RUN=1
       ;;
     \? )
       usage
@@ -46,17 +57,65 @@ fi
 # Function to process docker-compose.yml files
 process_docker_compose() {
   local file=$1
+  local volume_keys
+  local volume_key
 
   # Extract top-level volume elements with external enabled
-  volumes=$(yq e '.volumes | to_entries | map(select(.value.external == true)) | .[].key' "$file")
+  volume_keys=$(yq '.volumes | to_entries | map(select(.value.external == true)) | .[].key' "${file}")
 
-  for volume in $volumes; do
-    # Check if volume has the specific label
-    label_value=$(yq e ".volumes.$volume.labels[\"de.panzer1119.docker.volume.cifs.share\"]" "$file")
+  # Iterate over the volume keys
+  for volume_key in volume_keys; do
+    local labels
 
-    if [ "$label_value" != "null" ]; then
-      # Call create_docker_cifs_volume.sh with the necessary parameters
-      ./create_docker_cifs_volume.sh -n "$volume" -a "$address" -s "$label_value" -u "$username" -p "$password"
+    # Get the label array for the volume
+    labels=$(yq ".volumes.${volume_key}.labels" "${file}")
+
+    # If the labels are an empty array, empty, or null, skip
+    if [ "${labels}" == "[]" ] || [ -z "${labels}" ] || [ "${labels}" == "null" ]; then
+      continue
+    fi
+
+    local volume_name
+    local share_name
+
+    # Get the volume name
+    volume_name=$(yq -r ".volumes.${volume_key}.name" "${file}")
+
+    # Convert the label array to a dictionary
+    labels=$(echo "${labels}" | jq -r '. | map(split("=")) | map({(.[0]): .[1]}) | add')
+
+    # Check if volume has the specific label "de.panzer1119.docker.volume.cifs.share"
+    share_name=$(echo "${labels}" | jq -r '.["de.panzer1119.docker.volume.cifs.share"]')
+
+    # If the share name is null or empty, skip
+    if [ -z "${share_name}" ] || [ "${share_name}" == "null" ]; then
+      continue
+    fi
+
+    # If quiet mode is not enabled, display the volume name and share name
+    if [ -z "${QUIET}" ]; then
+      echo "Found volume '${volume_name}' with share name '${share_name}' in '${file}'"
+    fi
+
+    local command="bash \"${CREATE_CIFS_VOLUME_SCRIPT_FILE}\" -n \"${volume_name}\" -a \"${address}\" -s \"${share_name}\" -u \"${username}\" -p \"${password}\" -e"
+
+    # If quiet mode is enabled, suppress output
+    if [ -n "${QUIET}" ]; then
+      command="${command} -q"
+    fi
+
+    # If dry run is enabled, display the command
+    if [ -n "${DRY_RUN}" ]; then
+      # If quiet mode is not enabled, display the command
+      if [ -z "${QUIET}" ]; then
+        echo "${command}"
+      fi
+      continue
+    fi
+
+    # Create the CIFS volume
+    if ! eval "${command}"; then
+      exit 1
     fi
   done
 }
