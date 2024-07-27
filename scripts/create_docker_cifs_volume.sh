f9a9a39434d3de5599c376423a675f4696a56d48
Paul Hagedorn <panzer1119@gmail.com>
2024-07-28 00:42:57 +0200

    feat: add option dry run and verbose to create_docker_cifs_volume.sh

--- scripts/create_docker_cifs_volume.sh
+++ scripts/create_docker_cifs_volume.sh
@@ -2,36 +2,42 @@
 
 # Default values
 SMB_VERSION="3.0"
+VERBOSE=0
 EXIT_GRACEFULLY_IF_EXISTS=0
+QUIET=0
 QUIET=0
 
 # Function to display usage
 usage() {
-  echo "Usage: ${0} -n <volume_name> -a <address> -s <share_name> -u <username> -p <password> [-v <smb_version>] [-i <uid>] [-g <gid>] [-e] [-q]"
+  echo "Usage: ${0} -n <volume_name> -a <address> -s <share_name> -u <username> -p <password> [-V <smb_version>] [-i <uid>] [-g <gid>] [-v] [-N] [-e] [-q]"
   echo "  -n <volume_name>: Name of the Docker volume to create"
   echo "  -a <address>: Address of the CIFS/SMB server"
   echo "  -s <share_name>: Name of the shared folder on the server"
   echo "  -u <username>: Username for authentication"
   echo "  -p <password>: Password for authentication"
-  echo "  -v <smb_version>: Optional. SMB version (default: 3.0)"
+  echo "  -V <smb_version>: Optional. SMB version (default: 3.0)"
   echo "  -i <uid>: Optional. UID for file access (default: not set)"
   echo "  -g <gid>: Optional. GID for file access (default: not set)"
+  echo "  -v: Optional. Verbose mode"
+  echo "  -N: Optional. Dry run"
   echo "  -e: Optional. Exit gracefully if volume already exists (default: exit with error)"
   echo "  -q: Optional. Quiet mode"
   exit 1
 }
 
 # Parse options
-while getopts "n:a:s:u:p:v:i:g:eq" opt; do
+while getopts "n:a:s:u:p:V:i:g:vNeq" opt; do
   case ${opt} in
     n) VOLUME_NAME="${OPTARG}" ;;
     a) ADDRESS="${OPTARG}" ;;
     s) SHARE_NAME="${OPTARG}" ;;
     u) USERNAME="${OPTARG}" ;;
     p) PASSWORD="${OPTARG}" ;;
-    v) SMB_VERSION="${OPTARG}" ;;
+    V) SMB_VERSION="${OPTARG}" ;;
     i) USER_ID="${OPTARG}" ;;
     g) GROUP_ID="${OPTARG}" ;;
+    v) VERBOSE=1 ;;
+    N) DRY_RUN=1 ;;
     e) EXIT_GRACEFULLY_IF_EXISTS=1 ;;
     q) QUIET=1 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2
@@ -68,13 +74,25 @@ if [[ -n ${GROUP_ID} ]]; then
   DOCKER_CMD="${DOCKER_CMD},gid=${GROUP_ID}"
 fi
 
-# Execute Docker command
+# If quiet is disabled, display the message
 if [[ ${QUIET} -eq 0 ]]; then
   echo "Creating Docker CIFS volume '${VOLUME_NAME}' pointing to '${ADDRESS}/${SHARE_NAME}'..."
-  echo "${DOCKER_CMD}"
 fi
+
+# If verbose is enabled, display the docker command
+if [[ ${VERBOSE} -eq 1 ]]; then
+  echo "Docker command: ${DOCKER_CMD}"
+fi
+
+# If dry run is enabled, exit
+if [[ ${DRY_RUN} -eq 1 ]]; then
+  exit 0
+fi
+
+# Execute Docker command
 eval "${DOCKER_CMD}"
 
+# Display success message if not in quiet mode
 if [[ ${QUIET} -eq 0 ]]; then
   echo "Docker CIFS volume '${VOLUME_NAME}' created successfully."
 fi
