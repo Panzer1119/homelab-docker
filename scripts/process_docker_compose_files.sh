865d1ccd7c7cce9532109930f905f29e07f8bfd1
Paul Hagedorn <panzer1119@gmail.com>
2024-11-24 16:54:14 +0100

    chore: fix typo in script process_docker_compose_files.sh

--- scripts/process_docker_compose_files.sh
+++ scripts/process_docker_compose_files.sh
@@ -356,7 +356,7 @@ process_docker_compose() {
       # Create a dictionary with the volume name as the key and another dictionary with the key driver
       volume_dictionaries=$(echo "${volume_dictionaries}" | jq -r ".[\"${volume_name}\"] |= . + {\"driver\": \"${volume_driver}\"}")
 
-      # Extract the key from the volume label key (the second part after "de.panzer1119.docker.volume.")
+      # Extract the key from the volume label key (the third part after "de.panzer1119.docker.volume.")
       key=$(echo "${volume_label_key}" | sed -E 's/^de\.panzer1119\.docker\.volume\.(.*)\.(cifs|sshfs|rclone).(type|host|port|path|share|username|password)$/\3/')
 
       # If the key is empty or null, skip
